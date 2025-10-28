ef68ec262a870845477b0b91012ef18eea4d1973
prlouis_mydev <pierrereama.louis@gmail.com>
2025-10-28 16:38:52 +0000

    Fix imports issue

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,6 +1,6 @@
 import postgres from "postgres"; // npm:postgres
 import { drizzle } from "drizzle-orm/postgres-js";
-import { CONFIG } from "@/config/config.ts";
+import { CONFIG } from "@config";
 
 const sql = postgres(CONFIG.db.url, { ssl: "prefer" });
 export const db = drizzle(sql);
